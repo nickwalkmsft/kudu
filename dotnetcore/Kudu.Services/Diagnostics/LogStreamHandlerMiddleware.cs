e216369c0d9e8e59ca7aba1be23cac029de33cc1
Nick Walker <nickwalk@microsoft.com>
2018-01-31 10:10:44 -0800

    logstreaming wip

---
+++ dotnetcore/Kudu.Services/Diagnostics/LogStreamHandlerMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Kudu.Contracts.Tracing;
+using Kudu.Core.Tracing;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kudu.Services.Performance
+{
+    public class LogStreamHandlerMiddleware
+    {
+        public LogStreamHandlerMiddleware(RequestDelegate next)
+        {
+        }
+        public Task Invoke(HttpContext context, LogStreamManager manager, ITracer tracer)
+        {
+            using (tracer.Step("LogStreamHandlerMiddleware.Invoke"))
+            {
+                if (string.Equals(context.Request.Method, "GET", StringComparison.OrdinalIgnoreCase))
+                {
+                    return manager.ProcessRequest(context);
+                }
+                else
+	            {
+                    context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                    return Task.CompletedTask;
+                }
+            }
+        }
+    }
+}
